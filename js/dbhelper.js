8b0bfac3f2413b2fb62e614c9d39cd35110965aa
Jeonghwan Lee <jeonghwanlee@Jeonghwanui-MacBook-Pro.local>
2018-03-14 22:43:09 +0900

    Update sw.js and dbhelper.js to implement service worker(install,fetch, activate)

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -1,13 +1,12 @@
 //Register service worker
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register('/sw.js').then(function() {
-    console.log('Registration Worked!');
+  navigator.serviceWorker.register('/js/sw.js').then(function() {
+    console.log('Registration worked!');
   })
   .catch(function () {
-    console.log('Registration Failed!');
+    console.log('Registration failed!');
   });
-} 
-else {
+} else {
   console.log('Service worker is not supported in this browser');
 }
 
